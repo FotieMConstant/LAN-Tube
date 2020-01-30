26ce57a069e8ca01557f31ac885f7007dc82d3f9
FotieMConstant <fotiemoghommahieconstant@gmail.com>
2020-01-30 16:50:37 +0100

    minor bug fixes and performance improvement

--- upload.php
+++ upload.php
@@ -135,7 +135,7 @@
     <!-- Footer -->
     <footer class="py-5 bg-dark">
       <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; LAN Tube 2018</p>
+      <p class="m-0 text-center text-white">Copyright &copy; LAN Tube 2018 - <?php echo date("Y"); ?>.</p>
       </div>
       <!-- /.container -->
     </footer>
